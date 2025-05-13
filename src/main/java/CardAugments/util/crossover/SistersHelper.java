d840e8698c02c5ef05d7c85d36f266dd7e25b250
Evans Chen <evansschen@gmail.com>
2025-05-13 16:50:19 -0700

    Remove Starlight Sisters mod patch
    Causing build issues since the jar isn't obtainable anymore

--- src/main/java/CardAugments/util/crossover/SistersHelper.java
+++ src/main/java/CardAugments/util/crossover/SistersHelper.java
@@ -1,14 +1,17 @@
 package CardAugments.util.crossover;
 
-import Starlight.ui.ProjectedCardManager;
+// import Starlight.ui.ProjectedCardManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 import java.util.ArrayList;
 
 public class SistersHelper {
     public static ArrayList<AbstractCard> projectedCards() {
+        /*
         ArrayList<AbstractCard> cards = new ArrayList<>(ProjectedCardManager.cards.group);
         cards.addAll(ProjectedCardManager.renderQueue.group);
         return cards;
+        */
+        return new ArrayList<>();
     }
 }
